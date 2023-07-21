08bff14bd8414ff6c5176f581b4d6509012e8c08
ismum <ichowdhury4@fordham.edu>
2023-07-21 16:44:14 -0400

    feed finished

--- src/app/api/subreddit/post/create/route.ts
+++ src/app/api/subreddit/post/create/route.ts
@@ -6,15 +6,16 @@ import { z } from 'zod'
 export async function POST(req: Request) {
   try {
     const body = await req.json()
-    const { subredditId, title, content } = PostValidator.parse(body)
-    
+
+    const { title, content, subredditId } = PostValidator.parse(body)
+
     const session = await getAuthSession()
 
     if (!session?.user) {
       return new Response('Unauthorized', { status: 401 })
     }
 
-    // check if user has already subscribed to subreddit
+    // verify user is subscribed to passed subreddit id
     const subscription = await db.subscription.findFirst({
       where: {
         subredditId,
@@ -23,30 +24,26 @@ export async function POST(req: Request) {
     })
 
     if (!subscription) {
-      return new Response("You must subscribe to the sub-breddit", {
-        status: 400,
-      })
+      return new Response('Subscribe to post', { status: 403 })
     }
 
-    // create subreddit and associate it with the user
     await db.post.create({
-        data: {
-            title,
-            content,
-            authorId: session.user.id,
-            subredditId,
-        },
+      data: {
+        title,
+        content,
+        authorId: session.user.id,
+        subredditId,
+      },
     })
 
     return new Response('OK')
   } catch (error) {
-    (error)
     if (error instanceof z.ZodError) {
       return new Response(error.message, { status: 400 })
     }
 
     return new Response(
-      'Could not subscribe to subreddit at this time. Please try later',
+      'Could not post to subreddit at this time. Please try later',
       { status: 500 }
     )
   }
